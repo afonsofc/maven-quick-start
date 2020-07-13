ad4941a9ba3866d2aa45583965eac1277936fbe0
afonsofc <afonsofc@hotmail.com>
2020-07-13 19:33:02 +0100

    Changed java version in pom.xml

--- src/main/java/example/afonso/training/Application.java
+++ src/main/java/example/afonso/training/Application.java
@@ -1,14 +1,23 @@
 package example.afonso.training;
 
+import java.util.ArrayList;
+
 public class Application {
     
+	private ArrayList<String> messages;
+	
     public Application() {
         System.out.println ("Inside Application");
+		messages = new ArrayList<>();
+		messages.add("Hello Maven");
     }
 
     // method main(): ALWAYS the APPLICATION entry point
     public static void main (String[] args) {
     	System.out.println ("Starting Application");
-	Application app = new Application();
+		Application app = new Application();
+		for(String str: app.messages){
+			System.out.println (str);
+		}
     }
 }
